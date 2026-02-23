b13ac2d702ecb7c68b1db06525ac83b24bbc262e
IbtissemSghayri <ibtissem.sghayri@etudiant-enit.utm.tn>
2026-02-23 13:08:20 +0100

    Add palindrome check in Translation.cpp
    Implement a program to check if a string is a palindrome by comparing it to its reverse.

---
+++ 41A - Translation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    string n,s;
+    cin >>n>>s;
+
+    int len=n.length();
+    int i=0;
+    bool ok=true;
+
+    while (ok==true && i<len){
+        if (n[i]==s[len-i-1a]){
+            i++;
+        }
+        else {
+            break;
+        }
+    }
+    if (i==len){
+        cout <<"YES"<<endl;
+    }
+    else {
+        cout <<"NO"<<endl;
+    }
+
+
+
+
+    return 0;
+}
